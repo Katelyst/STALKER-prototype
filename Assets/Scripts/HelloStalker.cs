b9cadac27c92d4606d3d98cbb37a8d672fbc03a1
Sebastiaan <wiggler@live.nl>
2022-03-14 23:43:35 +0100

    Test update to hellostalker.cs
    Seeing if everything is working

--- Assets/Scripts/HelloStalker.cs
+++ Assets/Scripts/HelloStalker.cs
@@ -7,6 +7,7 @@ public class HelloStalker : MonoBehaviour
     void Awake()
     {
         Debug.Log("Hello Stalker. If you see this it means everything *should* be g00d");
+        //test
     }
 
 }
