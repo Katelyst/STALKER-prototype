3c8de902933380ae26d62b069a655bb2cfaaa15e
Jochem <joaghim.beltman@gmail.com>
2022-03-14 20:39:31 +0100

    Add starter assets & leantween, add prefab test for source control

---
+++ Assets/Scripts/HelloStalker.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloStalker : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Hello Stalker. If you see this it means everything *should* be g00d");
+    }
+
+}
